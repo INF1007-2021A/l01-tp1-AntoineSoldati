591cb526e1f0c5e1d4e70f29f510ad78eb8c95df
AntoineSoldati <90266863+AntoineSoldati@users.noreply.github.com>
2021-09-18 21:15:16 -0400

    Exercise 5

--- exercice5.py
+++ exercice5.py
@@ -1,10 +1,9 @@
 def pointDeRencontre(v1, v2, distance):
     # TODO faites les calculs intermediaires, vous pouvez initialiser des variables locales.
+    delta_Temps = distance/(v1+v2)
     # TODO calculer la position de rencontre, assignez la valeur à la variable "positionRencontre"
-    positionRencontre = (v1*distance)/(2*v2)
-
+    positionRencontre = v1 * delta_Temps
     return positionRencontre
-
 if __name__ == '__main__':
     v1 = int(input("Entrez v1: "))
     v2 = int(input("Entrez v2: "))
